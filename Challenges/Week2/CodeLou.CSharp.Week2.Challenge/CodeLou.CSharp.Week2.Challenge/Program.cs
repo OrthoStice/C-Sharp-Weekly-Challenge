5cbf38e0a148cf189639df9d4b1123197e96ec29
Shelley Hicklin <shelleyhicklin@hotmail.com>
2016-09-01 22:32:24 -0400

    Task1-3 complete

--- Challenges/Week2/CodeLou.CSharp.Week2.Challenge/CodeLou.CSharp.Week2.Challenge/Program.cs
+++ Challenges/Week2/CodeLou.CSharp.Week2.Challenge/CodeLou.CSharp.Week2.Challenge/Program.cs
@@ -1,48 +1,24 @@
 ﻿using System;
-
 namespace CodeLou.CSharp.Week2.Challenge
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // Task 1:
-            // The Console class is in the System namespace in the .Net framework. Add a "using" statement
-            // to the top of this file to import the System namespace so that the C# compiler knows where
-            // to look for the Console class.
-
-            // Task 2:
-            // This application won't build because of a syntax error. Fix the code below to make it build.
-            // Hint: In Visual Studio, you can build the project by selecting "Build Solution" from the
-            //       build menu, or by pressing <Ctrl>+<Shift>+B.
             Console.WriteLine("Welcome to the Code Louisville C# week 2 code challenge!");
             Console.WriteLine("Press <Enter> to begin...");
             Console.ReadLine();
-
             Console.WriteLine("This is the launch application for the first human mission to Mars.");
             Console.Write("Enter the number of seconds you would like to count down from: ");
-
-            // Task 3:
-            // Capture the number of seconds that the user would like to count down before liftoff.
-            // Hint: You should use another method of the Console class and store the output into a
-            //       variable to use later.
-
             var countDownEntry = Console.ReadLine();
-
-
             // Task 4:
             // Write a condition to test whether the number that they entered is less than or equal to zero.
             // Call the IsLessThanOrEqualToZero() method below, passing the user's number as a parameter.
             // If the result is true, write "Please enter a positive number." to the console.
             // Hint: The input that you captured is currently a string type. You will have to "parse" it
             //       as a different type in order to pass it to the IsLessThanOrEqualToZero function.
-
-            public bool IsLessThanOrEqualToZero(countDownEntry)
-            {
-             if countDownEntry.P
-            }
-
-
+            public IsLessThanOrEqualToZero(countDownEntry) { }
+           
             // Task 5:
             // Add an "else" block to the condition from Task 4. This should be run in the case that the
             // number is greater than zero. Write each number to the console, counting backwards, from the 
