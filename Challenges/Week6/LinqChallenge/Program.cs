ce376e1e8ecc2613f350a87a72f77f99eb437d11
Shelley Hicklin <shelleyhicklin@hotmail.com>
2016-10-03 14:51:07 -0400

    1st 2 queries--query syntax and method

--- Challenges/Week6/LinqChallenge/Program.cs
+++ Challenges/Week6/LinqChallenge/Program.cs
@@ -25,9 +25,23 @@ namespace LinqChallenge
             NewQuestion();
             Console.WriteLine("What are the airports in Louisville?");
 
+            var airportsInLouisville =
+                from a in Airports
+                where a.City == "Louisville"
+                select a;
+
+            foreach (var a in airportsInLouisville)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(a.ToString());
+                }
+
             NewQuestion();
 
             Console.WriteLine("What is the Latitude/Longitude of KSDF?");
+
+            var latitudeAndLongitude = Airports
+                .Where(a => a.ICAO == "KSDF");
 
             NewQuestion();
 
@@ -171,5 +185,37 @@ namespace LinqChallenge
             public int IncorporatedYear { get; set; }
             public string County { get; set; }
         }
+    }
+}
+class QueryVMethodSyntax
+{
+    static void Main()
+    {
+        int[] numbers = { 5, 10, 8, 3, 6, 12 };
+
+        //Query syntax:
+        IEnumerable<int> numQuery1 =
+            from num in numbers
+            where num % 2 == 0
+            orderby num
+            select num;
+
+        //Method syntax:
+        IEnumerable<int> numQuery2 = numbers.Where(num => num % 2 == 0).OrderBy(n => n);
+
+        foreach (int i in numQuery1)
+        {
+            Console.Write(i + " ");
+        }
+        Console.WriteLine(System.Environment.NewLine);
+        foreach (int i in numQuery2)
+        {
+            Console.Write(i + " ");
+        }
+
+        // Keep the console open in debug mode.
+        Console.WriteLine(System.Environment.NewLine);
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
     }
 }
