2e5b613871c9800e4a72a7ba9fdb8856324409ba
Shelley Hicklin <shelleyhicklin@hotmail.com>
2016-10-03 15:32:47 -0400

    We're building now and latitude longitude working

--- Challenges/Week6/LinqChallenge/Program.cs
+++ Challenges/Week6/LinqChallenge/Program.cs
@@ -43,6 +43,12 @@ namespace LinqChallenge
             var latitudeAndLongitude = Airports
                 .Where(a => a.ICAO == "KSDF");
 
+            foreach (var ll in latitudeAndLongitude)
+            {
+                Console.WriteLine("Latitude: " + ll.Latitude.ToString());
+                Console.WriteLine("Longitude: " + ll.Longitude.ToString());
+            }
+
             NewQuestion();
 
             Console.WriteLine("How many routes originate in in louisville? ");
@@ -185,37 +191,5 @@ namespace LinqChallenge
             public int IncorporatedYear { get; set; }
             public string County { get; set; }
         }
-    }
-}
-class QueryVMethodSyntax
-{
-    static void Main()
-    {
-        int[] numbers = { 5, 10, 8, 3, 6, 12 };
-
-        //Query syntax:
-        IEnumerable<int> numQuery1 =
-            from num in numbers
-            where num % 2 == 0
-            orderby num
-            select num;
-
-        //Method syntax:
-        IEnumerable<int> numQuery2 = numbers.Where(num => num % 2 == 0).OrderBy(n => n);
-
-        foreach (int i in numQuery1)
-        {
-            Console.Write(i + " ");
-        }
-        Console.WriteLine(System.Environment.NewLine);
-        foreach (int i in numQuery2)
-        {
-            Console.Write(i + " ");
-        }
-
-        // Keep the console open in debug mode.
-        Console.WriteLine(System.Environment.NewLine);
-        Console.WriteLine("Press any key to exit");
-        Console.ReadKey();
     }
 }
